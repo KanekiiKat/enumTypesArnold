3a6c73b0f941cacd7cc7b50f490f25ecaf7fcfd9
KanekiiCat <alex123viri@gmail.com>
2025-04-01 17:21:48 +0200

    test(Planeta): Añadir PlanetaConstructortest

---
+++ src/test/java/edu/estatuas/arnoldEnumType/test/ArnoldMainTest.java
@@ -0,0 +1,44 @@
+package edu.estatuas.arnoldEnumType.test;
+
+
+
+import static  org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.estatuas.arnoldEnumType.logica.Planeta;
+
+
+public class ArnoldMainTest {
+
+    public static String[] planetas;
+
+	@BeforeEach
+	public void CreacionArrayPlanetasSetup() {
+		planetas = new String[8];
+		int planetasIncluidos = 0;
+		for (Planeta planeta : Planeta.values()) {
+			planetas[planeta.ordinal()] = planeta.name();
+			planetasIncluidos += 1;
+		}
+		assertThat(planetasIncluidos).isEqualTo(Planeta.values().length);
+		assertThat(planetas).doesNotContainNull();
+	}
+
+    	@Test
+	public void PlanetaConstructorTest() {
+		// me aseguro de que los metodos de la api
+		// de los Enum Types se comportan como espero
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta).isInstanceOf(Planeta.class);
+		assertThat(planeta.ordinal()).isZero();
+		assertThat(planeta.name()).isEqualToIgnoringCase("MERCURY");
+		assertThat(Planeta.valueOf(planeta.name())).isEqualTo(Planeta.MERCURY);
+		assertThat(planeta.compareTo(Planeta.MERCURY)).isZero();
+		assertThat(planeta.toString()).isEqualToIgnoringCase("MERCURY");
+		assertThat(planeta.equals(Planeta.MERCURY)).isTrue();
+		assertThat(Planeta.values()[0]).isEqualTo(planeta);
+	}
+
+    
+}
