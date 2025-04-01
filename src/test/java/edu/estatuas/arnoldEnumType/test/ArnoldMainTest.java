191928271c842d859a8c5aca27e51520948a662c
KanekiiCat <alex123viri@gmail.com>
2025-04-01 17:27:39 +0200

    test(Planeta): Añadir PlanetaGetRadioTest()

--- src/test/java/edu/estatuas/arnoldEnumType/test/ArnoldMainTest.java
+++ src/test/java/edu/estatuas/arnoldEnumType/test/ArnoldMainTest.java
@@ -40,5 +40,10 @@ public class ArnoldMainTest {
 		assertThat(Planeta.values()[0]).isEqualTo(planeta);
 	}
 
+    @Test
+	public void PlanetaGetRadioTest() {
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta.getRadio()).isEqualTo(2.4397e+6);
+	}
     
 }
